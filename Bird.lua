ca95862efeef150a6e0d6c843bda75279cdf88ab
Shivannand Patil <shivanandpatil.tech@gmail.com>
2026-06-13 21:24:15 +0530

    [#15] Add collision
    - Add collision
    - pause game on collision
    
    Signed-off-by: Shivannand Patil <shivanandpatil.tech@gmail.com>

--- Bird.lua
+++ Bird.lua
@@ -20,6 +20,15 @@ function Bird:update(dt)
     self.y = self.y + self.dy * dt
 end
 
+function Bird:collides(pipe)
+    if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then  
+        if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
+            return true
+        end
+    end
+    return false 
+end    
+
 function Bird:render()
     love.graphics.draw(self.image, self.x, self.y)
 end
